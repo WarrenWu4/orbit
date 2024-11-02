25c5c1b2a3496943a77862432c4a8044e6843744
redninjale <redninjale@gmail.com>
2024-11-02 14:05:59 -0500

    explore page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'arcade': '0px 0px 20px 10px rgba(255,255,255,0.71)',
+      },
+      backgroundImage: {
+        'arcade': "url('/path/to/arcade-background.png')",
+      },
+    },
   },
   plugins: [],
 }
