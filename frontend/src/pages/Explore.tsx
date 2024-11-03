ebd6089355fc92828615ed24209d303c43f9be69
Tejas Kalpathi <tejaskalpathi@gmail.com>
2024-11-02 19:49:11 -0500

    updated cards

--- frontend/src/pages/Explore.tsx
+++ frontend/src/pages/Explore.tsx
@@ -5,100 +5,108 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 
 export default function Explore() {
+  const videos = [
+    { id: "ballet", title: "Ballet", videoURL: "/videos/ballet.mov" },
+    { id: "afro", title: "Afro", videoURL: "/videos/afro.mov" },
+    { id: "footloose", title: "Footloose", videoURL: "/videos/footloose.mov" },
+    { id: "dynamite", title: "Dynamite", videoURL: "/videos/dynamite.mov" },
+    { id: "bhangra", title: "Bhangra", videoURL: "/videos/bhangra.mov" },
+    { id: "salsa", title: "Salsa", videoURL: "/videos/salsa.mov" },
+    { id: "butter", title: "Butter", videoURL: "/videos/butter.mov" },
+    { id: "drill", title: "Drill", videoURL: "/videos/drill.mov" },
+    { id: "rasputin", title: "Rasputin", videoURL: "/videos/rasputin.mov" },
+  ];
 
-    const [videos, setVideos] = useState<any[]>([]);
+//   useEffect(() => {
+//     async function fetchVideos() {
+//       // fetch videos from the database
+//       const querySnapshot = await getDocs(collection(db, "videos"));
+//       const tempVideos: any[] = [];
+//       querySnapshot.forEach((doc) => {
+//         tempVideos.push({
+//           id: doc.id,
+//           ...doc.data(),
+//         });
+//       });
+//       setVideos(tempVideos);
+//     }
 
-    useEffect(() => {
+//     fetchVideos();
+//   }, []);
 
-        async function fetchVideos() {
-            // fetch videos from the database
-            const querySnapshot = await getDocs(collection(db, "videos"));
-            const tempVideos: any[] = [];
-            querySnapshot.forEach((doc) => {
-                tempVideos.push({
-                    id: doc.id,
-                    ...doc.data()
-                });
-            });
-            setVideos(tempVideos);
-        }
+  return (
+    <Page>
+      <div className="w-full h-full flex flex-col items-center">
+        <h1 className="text-white font-bold text-3xl">trending dances</h1>
+        <span className="mb-8">play a trending dance</span>
 
-        fetchVideos();
-
-    }, [])
-
-    return (
-        <Page>
-
-            <div className="w-full h-full flex flex-col items-center">
-
-                <h1 className="text-white font-bold text-3xl">trending dances</h1>
-                <span className="mb-8">play a trending dance</span>
-
-                <div className="grid grid-cols-3 gap-y-16 gap-x-16 mx-16">
-                    {videos.length !== 0 &&
-                        videos.map((video, index) => (
-                            <VideoCard key={index}
-                                videoId={video.id}
-                                videoUrl={video.videoURL}
-                                videoTitle={video.title}
-                                hearts={video.hearts}
-                            />
-                        ))
-                    }
-                </div>
-
-            </div>
-
-        </Page>
-    )
+        <div className="grid grid-cols-3 gap-y-16 gap-x-16 mx-16">
+          {videos.length !== 0 &&
+            videos.map((video, index) => (
+              <VideoCard
+                key={index}
+                videoId={video.id}
+                videoUrl={video.videoURL}
+                videoTitle={video.title}
+                hearts={video.hearts}
+              />
+            ))}
+        </div>
+      </div>
+    </Page>
+  );
 }
 
 function VideoCard(props: any) {
-    const [isHovered, setIsHovered] = useState(false);
-    return (
-        <div className="flex flex-col border-4 border-white px-4 py-2 hover:shadow-arcade hover:cursor-pointer relative gap-1"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-        >
-            <div className="absolute top-[-16px] right-[-16px] border-8 border-white"></div>
-            <div className="flex flex-row">
-                <div className="flex flex-1 flex-row gap-2 items-center">
-                    <div className="w-[15px]">
-                        <img src="/musical-note.png" alt="Music note" />
-                    </div>
-                    {/* <span>{props.musicTitle.length > 10 ? `${props.musicTitle.substring(0, 10)}...` : props.musicTitle}</span> */}
-                </div>
-                <a href={`/play/${props.videoId}`} className="flex flex-row gap-2 items-center underline text-sm">
-                    try it!
-                    <div className="w-[25px]">
-                        <img src="/joystick.png" alt="play" />
-                    </div>
-
-                </a>
-            </div>
-
-
-            <div className="video-container relative overflow-x-hidden">
-                <ReactPlayer
-                    url={props.videoUrl}
-                    playing={isHovered}
-                    height="100%"
-                    style={{ aspectRatio: '16/9', transform: 'translateX(-30%)' }}
-                />
-
-                {!isHovered && (
-                    <div className="absolute z-2 inset-0 bg-gray-700 bg-opacity-75 flex flex-col items-center justify-center text-white">
-                        <img src="/tap.png" alt="" />
-                        <span>Hover Me</span>
-                    </div>
-                )}
-            </div>
-            <div>{props.videoTitle}</div>
-            <div className="w-full flex flex-row">
-                {/* <span className="font-bold flex-1">{props.creator}</span> */}
-                <span><img src="" alt="" />{props.heart}</span>
-            </div>
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div
+      className="flex flex-col border-4 border-white px-4 py-2 hover:shadow-arcade hover:cursor-pointer relative gap-1"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="absolute top-[-16px] right-[-16px] border-8 border-white"></div>
+      <div className="flex flex-row">
+        <div className="flex flex-1 flex-row gap-2 items-center">
+          <div className="w-[15px]">
+            <img src="/musical-note.png" alt="Music note" />
+          </div>
+          {/* <span>{props.musicTitle.length > 10 ? `${props.musicTitle.substring(0, 10)}...` : props.musicTitle}</span> */}
         </div>
-    )
+        <a
+          href={`/play/${props.videoId}`}
+          className="flex flex-row gap-2 items-center underline text-sm"
+        >
+          try it!
+          <div className="w-[25px]">
+            <img src="/joystick.png" alt="play" />
+          </div>
+        </a>
+      </div>
+
+      <div className="video-container relative overflow-x-hidden">
+        <ReactPlayer
+          url={props.videoUrl}
+          playing={isHovered}
+          height="100%"
+          style={{ aspectRatio: "16/9", transform: "translateX(-30%)" }}
+        />
+
+        {!isHovered && (
+          <div className="absolute z-2 inset-0 bg-gray-700 bg-opacity-75 flex flex-col items-center justify-center text-white">
+            <img src="/tap.png" alt="" />
+            <span>Hover Me</span>
+          </div>
+        )}
+      </div>
+      <div>{props.videoTitle}</div>
+      <div className="w-full flex flex-row">
+        {/* <span className="font-bold flex-1">{props.creator}</span> */}
+        <span>
+          <img src="" alt="" />
+          {props.heart}
+        </span>
+      </div>
+    </div>
+  );
 }
