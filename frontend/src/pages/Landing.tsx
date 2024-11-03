9fce1447fa60cedba5d8ee829bd73f495edfb002
WarrenWu4 <warrenweiwu04@gmail.com>
2024-11-03 09:37:47 -0600

    umm styling... i think

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -19,8 +19,8 @@ function HeroSection() {
         </div>        
 
 
-        <p className="font-bold text-2xl mb-2">
-          Explore culture worldwide through the medium of{" "}
+        <p className="font-bold text-2xl mb-2 text-center">
+          Explore culture worldwide<br/>through the medium of{" "}
           <span className="underline text-bold">dance!</span>
         </p>
 
